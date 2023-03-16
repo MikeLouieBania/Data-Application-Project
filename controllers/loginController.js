d8ddc267c4e364df4d899d19a3db82b85e66242c
TamamoPasta <mmlsbania@tip.edu.ph>
2023-03-16 15:23:23 +0800

    Added Validation, Encryption, and Password Policy

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -8,11 +8,41 @@ const connection = mysql.createConnection({
   database: 'nodeapps'
 });
 
+function encrypt(text, shift) {
+  let result = '';
+  for (let i = 0; i < text.length; i++) {
+    let char = text.charAt(i);
+    // Check if character is a letter
+    if (/[a-zA-Z]/.test(char)) {
+      let code = text.charCodeAt(i);
+      // Uppercase letters
+      if (code >= 65 && code <= 90) {
+        char = String.fromCharCode(((code - 65 + shift) % 26) + 65);
+      }
+      // Lowercase letters
+      else if (code >= 97 && code <= 122) {
+        char = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+      }
+    }
+    // Check if character is a number or special character
+    else if (/[0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(char)) {
+      let code = text.charCodeAt(i);
+      char = String.fromCharCode((code + shift) % 128);
+    }
+    result += char;
+  }
+  return result;
+}
+
 exports.postLogin = (req, res) => {
   const { email, password } = req.body;
 
+  // Encrypting the input password
+  // Checks if the encrypted password matches that on the db
+  const encryptedPassword = encrypt(password, 3);
+
   // Execute a query to retrieve the user from the database
-  connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (err, results) => {
+  connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, encryptedPassword], (err, results) => {
     if (err) {
       console.error('Error executing query: ' + err.stack);
       return res.status(500).send('Error executing query');
--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+const validator = require('email-validator');
 
 // Create a MySQL connection
 const connection = mysql.createConnection({
@@ -8,6 +9,36 @@ const connection = mysql.createConnection({
   database: 'nodeapps'
 });
 
+function encrypt(text, shift) {
+  let result = '';
+  for (let i = 0; i < text.length; i++) {
+    let char = text.charAt(i);
+
+    // Checks if the input is letter
+    if (/[a-zA-Z]/.test(char)) {
+      let code = text.charCodeAt(i);
+
+      // Uppercase
+      if (code >= 65 && code <= 90) {
+        char = String.fromCharCode(((code - 65 + shift) % 26) + 65);
+      }
+
+      // Lowercase
+      else if (code >= 97 && code <= 122) {
+        char = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+      }
+    }
+
+    // Check if the input is a special character or number
+    else if (/[0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(char)) {
+      let code = text.charCodeAt(i);
+      char = String.fromCharCode((code + shift) % 128);
+    }
+    result += char;
+  }
+  return result;
+}
+
 exports.getRegisterPage = (req, res) => {
   res.render('register', { message: null });
 };
@@ -15,9 +46,23 @@ exports.getRegisterPage = (req, res) => {
 exports.registerUser = (req, res) => {
   const { email, password, usertype } = req.body;
 
+  // Email Validation
+  if (!validator.validate(email)) {
+    return res.render('register', { message: 'Invalid email address' });
+  }
+
+  // Password Validation
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/;
+  if (!passwordRegex.test(password)) {
+    return res.render('register', { message: 'Password must be: 8 characters minimum, at least one number, one lowercase letter, one uppercase letter, and one special character' });
+  }
+
+  // Encryption
+  const encryptedPassword = encrypt(password,3);
+
   // Insert user into MySQL database
   const query = `INSERT INTO users (email, password, usertype) VALUES (?, ?, ?)`;
-  connection.query(query, [email, password, usertype], (err, result) => {
+  connection.query(query, [email, encryptedPassword, usertype], (err, result) => {
     if (err) {
       console.error(err);
       res.render('register', { message: 'Error registering user' });
@@ -25,5 +70,4 @@ exports.registerUser = (req, res) => {
       res.render('login', { message: 'User successfully registered' });
     }
   });
-};
- 
+};  
